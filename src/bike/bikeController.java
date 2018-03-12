3facdf0c0eea2c91ea8a1cd2ff5405b3ffb9f3ef
aadneny <aadneny@stud.ntnu.no>
2018-03-12 12:07:34 +0100

    Merge branch 'master' of gitlab.stud.iie.ntnu.no:007/Elsykkelutleie
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

---
+++ src/bike/bikeController.java
@@ -0,0 +1,72 @@
+package bike;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import changescene.ChangeScene;
+
+public class bikeController {
+
+    @FXML
+    private Button editBikeBtn;
+
+    @FXML
+    private Button infoBikeBtn;
+
+    @FXML
+    private Button deleteBikeBtn;
+
+    @FXML
+    private Button repairBikeBtn;
+
+    @FXML
+    private Button mainViewBtn;
+
+    @FXML
+    private Button newBikeBtn;
+
+    @FXML
+    private Button editBikeTypesBtn;
+
+    @FXML
+    private TextField bikeIdField;
+
+    @FXML
+    void changeToBikeEditView(ActionEvent event) {
+
+    }
+
+    @FXML
+    void changeToBikeNewView(ActionEvent event) throws Exception {
+        ChangeScene cs = new ChangeScene();
+        cs.setScene(event, "/bike/bikeNew/bikeNewView.fxml");
+    }
+
+    @FXML
+    void changeToBikeRepairView(ActionEvent event) {
+
+    }
+
+    @FXML
+    void changeToDockScene(ActionEvent event) {
+
+    }
+
+    @FXML
+    void changeToMainView(ActionEvent event) throws Exception {
+        ChangeScene cs = new ChangeScene();
+        cs.setScene(event, "/main/mainView.fxml");
+    }
+
+    @FXML
+    void changetoBikeInfoView(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteBike(ActionEvent event) {
+
+    }
+
+}
---
+++ src/bike/bikeNew/bikeNewController.java
@@ -0,0 +1,36 @@
+package bike.bikeNew;
+
+import changescene.ChangeScene;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class bikeNewController {
+
+    @FXML
+    private TextField makeField;
+
+    @FXML
+    private Button bikeViewBtn;
+
+    @FXML
+    private TextField priceField;
+
+    @FXML
+    private TextField buyDateField;
+
+    @FXML
+    private ComboBox<?> typeComboBox;
+
+    @FXML
+    private Button saveBtn;
+
+    @FXML
+    void changeToBikeView(ActionEvent event) throws Exception {
+        ChangeScene cs = new ChangeScene();
+        cs.setScene(event, "/bike/bikeView.fxml");
+    }
+
+}
