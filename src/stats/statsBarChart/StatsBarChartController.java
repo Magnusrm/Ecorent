b42b6dd116181ba6673ca3f595f67e3809bada7d
aadneny <aadneny@stud.ntnu.iie.no>
2018-04-17 13:17:28 +0200

    Added powerUsage to barchart

--- src/stats/statsBarChart/StatsBarChartController.java
+++ src/stats/statsBarChart/StatsBarChartController.java
@@ -7,8 +7,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
+import model.DockStatsModel;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 
 public class StatsBarChartController implements Initializable {
@@ -19,14 +21,19 @@ public class StatsBarChartController implements Initializable {
     @FXML
     private Label headerLbl;
 
+    private double[] days;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        DockStatsModel dsm = new DockStatsModel();
+        days = dsm.getDailyPowerUsage();
+        LocalDate tomorrow = LocalDate.now().plusDays(1);
         headerLbl.setText("");
         barChart.setLegendVisible(false);
         XYChart.Series set1 = new XYChart.Series<>();
 
-        for(int i = 0; i<7; i++){
-            set1.getData().add(new XYChart.Data("Dag " + i, 5 + i*i));
+        for(int i = 6; i>=0; i--){
+            set1.getData().add(new XYChart.Data("" + LocalDate.now().minusDays(i), days[i]));
         }
 
         barChart.getData().addAll(set1);
--- src/stats/statsPieChart/StatsPieChartController.java
+++ src/stats/statsPieChart/StatsPieChartController.java
@@ -10,8 +10,10 @@ import javafx.scene.chart.BarChart;
 import javafx.scene.chart.PieChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
+import model.DockStatsModel;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class StatsPieChartController implements Initializable {
@@ -22,16 +24,27 @@ public class StatsPieChartController implements Initializable {
     @FXML
     private Label headerLbl;
 
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        headerLbl.setText("header");
+
+        DockStatsModel dsm = new DockStatsModel();
+        ArrayList<int[]> checkouts = dsm.getMaxCheckouts();
+
+
+        headerLbl.setText("Daily Power Usage");
         pieChart.setLegendVisible(false);
         ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(
-                new PieChart.Data("dockname" + 5, 5 + 5*10),
-                new PieChart.Data("dockname" + 1, 5 + 1*10),
-                new PieChart.Data("dockname" + 2, 5 + 2*10),
-                new PieChart.Data("dockname" + 3, 5 + 3*10)
+
         );
+
+        for(int[] i : checkouts){
+           pieChartData.add(new PieChart.Data("Dock: " + i[0], i[1]));
+
+        }
+
+
+        // datos.add(new Tabla(v) );
 /*
         for(int i = 0; i<7; i++){
             new*/
