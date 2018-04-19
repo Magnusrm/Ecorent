b2b2f62920f2d25846e553ec08f2a9bce10b7c38
iliar <ilia.rad.saadat@hotmail.com>
2018-04-19 09:16:22 +0200

    Commit before pull from view

--- src/control/Password.java
+++ src/control/Password.java
@@ -4,6 +4,8 @@ import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 import java.security.SecureRandom;
+import java.util.Iterator;
+
 import org.apache.commons.codec.binary.Base64;
 
 /**
@@ -24,8 +26,15 @@ public class Password {
 
     public Password(){}//Default constructor
 
-    //Computes a salted PBKDF2 hash of given password(text).
-    //This is the hash we store in the database.
+    /**
+     * Computes a salted PBKDF2 hash of given password(text).
+     * The hash's first part is the salt we use to hash the password with.
+     * This and the hashed result is what we store in the database.
+     * The salt and the hashed result are separated by a char of our choice
+     * (here we use "$").
+     * @param password is an object of String.java
+     * @return String of the salt and hashed result
+     */
     public static String hashPassword(String password){
         try {
             byte[] salt = SecureRandom.getInstance("SHA1PRNG").generateSeed(saltLen);
@@ -38,13 +47,24 @@ public class Password {
         }//end catch
     }//end method
 
-    //public method to check if password corresponds
-    //to a stored salted hash of the password.
+    /**
+     * Public method to check if password corresponds
+     * to a stored salted hash of the password.
+     * @param password is an object of String.java
+     * @param stored is an object of String.java
+     * @return boolean true if the passwords correspond.
+     */
     public static boolean check(String password, String stored){
+        if(password == null || password.length() == 0 ||
+                stored == null || stored.length() == 0)throw new IllegalArgumentException("Password cannot" +
+                " be empty!");
         String[] findSaltAndHash = stored.split("\\$");
-        if(findSaltAndHash.length != 2){
-            throw new IllegalStateException("Lagret passord har formen 'salt$hash'.");
+        if(findSaltAndHash.length != 2){ //protection against the runtime exception
+            throw new IllegalStateException("Stored password has the form 'salt$hash'.");
         }//end if
+
+        //Hashing the written password with the same salt as the stored password.
+        //If the result is the stored hash the password is correct.
         try{
             String hashIn = hash(password,Base64.decodeBase64(findSaltAndHash[0]));
             return hashIn.equals(findSaltAndHash[1]);
@@ -55,11 +75,17 @@ public class Password {
         }//end catch
     }//end method
 
-    //private method to generate a hash,
-    //using PBKDF2 from Sun..
+
+    /**
+     * Private method to generate a hash,
+     * using PBKDF2 algorithm from Sun.
+     * @param password is an object of String.java
+     * @param salt is an array of bytes.
+     * @return String, which is the hash.
+     */
     private static String hash(String password, byte[] salt){
         if(password == null || password.length() == 0)throw new
-                IllegalArgumentException("Passord kan ikke være tomt");
+                IllegalArgumentException("Password cannot be empty");
         try{
             PBEKeySpec pbe = new PBEKeySpec(password.toCharArray(),salt, iterations,desiredKeyLen);//What we want hashed
             SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");//Hash algorithm
